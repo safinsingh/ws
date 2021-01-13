fba634c8409056a0c93bb3571adfc03c61b2a3cf
Safin Singh <42120904+safinsingh@users.noreply.github.com>
2021-01-13 15:36:12 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ const Home = (props: ProjectArr) => {
 			<h1>safin singh</h1>
 			<Socials links={socials} />
 			<h2>web developer &amp; cybersecurity competitor.</h2>
-			<h3>i write good code. here&apos;s some of my best work.</h3>
+			<h3>here&apos;s some of my best work.</h3>
 			<Projects projects={projects} />
 			<Cursor n={1} state={cursorPosOne} />
 			<Cursor n={2} state={cursorPosTwo} />
